7ed6a5eab72eb76df8f44f682a322f4fbcb35470
joaochencci <joaochencci@gmail.com>
2014-06-07 16:41:33 -0300

    Estrutura de telas já relacionadas com seus Controllers

---
+++ AngelHack/AngelHack/AHCreateEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  AHCreateEventViewController.h
+//  AngelHack
+//
+//  Created by Joao Victor Chencci Marques on 07/06/14.
+//  Copyright (c) 2014 devnup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHCreateEventViewController : UIViewController
+
+@end
---
+++ AngelHack/AngelHack/AHMyEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AHMyEventsViewController.h
+//  AngelHack
+//
+//  Created by Joao Victor Chencci Marques on 07/06/14.
+//  Copyright (c) 2014 devnup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHMyEventsViewController : UIViewController
+
+@end
