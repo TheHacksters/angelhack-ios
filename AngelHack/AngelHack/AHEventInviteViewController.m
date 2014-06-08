5e0eee3763a540d3cfcbb27d88c2fdbb9dddd96c
joaochencci <joaochencci@gmail.com>
2014-06-08 12:09:16 -0300

    InviteEvent implementada | Falta lógica de finalização do Evento

--- AngelHack/AngelHack/AHEventInviteViewController.m
+++ AngelHack/AngelHack/AHEventInviteViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "AHEventInviteViewController.h"
+#import "AHCreateEmailTableViewCell.h"
+#import "AHUtils.h"
 
-@interface AHEventInviteViewController ()
+@interface AHEventInviteViewController () <UITableViewDelegate, UITableViewDataSource, TableCellDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UIView *emailContainerView;
+@property (weak, nonatomic) IBOutlet UIButton *createButton;
+@property (weak, nonatomic) IBOutlet UITableView *emailTableView;
+
+@property (strong, nonatomic) NSMutableArray *emails;
 
 @end
 
@@ -26,7 +35,29 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    [self.view addGestureRecognizer:gestureRecognizer];
+    
+    self.emails = [[NSMutableArray alloc] init];
+    self.emailTableView.delegate = self;
+    self.emailTableView.dataSource = self;
+    
+    self.emailTableView.backgroundColor = [UIColor clearColor];
+    
+    //Change Placeholder Font Color
+    self.emailField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Email" attributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
+    
+    // Setting Textfield Border Color
+    self.emailContainerView.layer.borderColor = [UIColor colorWithRed:0.980392f green:0.980392f blue:0.980392f alpha:0.980392f].CGColor;
+    
+    // Setting Signup Button State Color
+    UIView *blueBackgroundView = [[UIView alloc] initWithFrame:self.createButton.frame];
+    blueBackgroundView.backgroundColor = [UIColor colorWithRed:0.41960784f green:0.5725490f blue:0.666666667f alpha:1.0f];
+    UIImage *blueBackground = [AHUtils imageFromView:blueBackgroundView];
+    [self.createButton setBackgroundImage:blueBackground forState:UIControlStateNormal];
+    
+    self.emailTableView.sectionFooterHeight = 0.0f;
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,15 +66,100 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+#pragma mark - Table View Delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    return 1;
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.emails.count;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
+}
+
+//- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+//    // This will create a "invisible" footer
+//    return 0.01f;
+//}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"TableCellID";
+    
+    AHCreateEmailTableViewCell *tablecell = (AHCreateEmailTableViewCell *)[self.emailTableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    tablecell.backgroundColor = [UIColor clearColor];
+    tablecell.emailLabel.text = [self.emails objectAtIndex:indexPath.row];
+    tablecell.delegate = self;
+    
+    return tablecell;
+}
+
+#pragma mark - Email List Manager
+- (IBAction)addEmailToList:(id)sender
+{
+    if (![self validEmail]) {
+        UIAlertView *alert = [[UIAlertView alloc] init];
+        
+        NSString *title = @"Email inválido";
+        NSString *message = @"Por favor, insira novamente o email.";
+        
+        alert = [alert initWithTitle:title message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+        return;
+    }
+    
+    [self.emails addObject: self.emailField.text];
+    self.emailField.text = @"";
+    
+    [self.emailTableView reloadData];
+}
+
+- (void)deleteButtonTappedOnCell:(id)sender {
+    NSIndexPath *indexpath = [self.emailTableView indexPathForCell:sender];
+    [self.emails removeObjectAtIndex:indexpath.row];
+    
+    [self.emailTableView reloadData];
+}
+
+- (IBAction)doneInviteCoworkers:(id)sender
+{
+    
+}
+
+#pragma mark - Support Methods
+- (BOOL)validEmail
+{
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    
+    if(![emailTest evaluateWithObject:self.emailField.text])
+    {
+        return NO;
+    }
+    return YES;
+}
+
+-(void)dismissKeyboard
+{
+    [self.emailField resignFirstResponder];
+    
+}
+
+#pragma mark - Status Bar Style
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - Dismiss Signup
+- (IBAction)popViewController:(id)sender
+{
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
 
 @end
